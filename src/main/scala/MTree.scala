ad6b7ca4d4de52625c69da2fe1612042dbe5421d
OT Chen <ot32em@gmail.com>
2014-12-03 13:49:09 +0800

    pass the test of node_count

--- src/main/scala/MTree.scala
+++ src/main/scala/MTree.scala
@@ -3,6 +3,9 @@ case class MLeaf[A](v: A) extends MTree[A]
 case class MBranch[A](lt: MTree[A], rt: MTree[A]) extends MTree[A]
 
 object MTree {
-  def node_count[A](t: MTree[A]): Int = 0
+  def node_count[A](t: MTree[A]): Int = t match {
+    case MBranch(left, right) => 1 + node_count(left) + node_count(right)
+    case MLeaf(v) => 1
+  }
 }
 
