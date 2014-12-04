5861e3d9e62b45203e521230d2f55e1cd862eee7
OT Chen <ot32em@gmail.com>
2014-12-04 14:03:29 +0800

    refactor map

--- src/main/scala/MTree.scala
+++ src/main/scala/MTree.scala
@@ -3,20 +3,20 @@ case class MLeaf[A](v: A) extends MTree[A]
 case class MBranch[A](lt: MTree[A], rt: MTree[A]) extends MTree[A]
 
 object MTree {
-  def node_count[A](t: MTree[A]): Int = fold(t,1)(1+_+_)((_,x)=>x) 
+  def node_count[A](t: MTree[A]): Int = fold(t)(a=>1)(1+_+_) 
 
-  def fold[A, B](t: MTree[A], z: B)(f: (B,B)=>B)(g: (A,B)=>B): B = t match {
-    case MBranch(left, right) => f(fold(left,z)(f)(g), fold(right,z)(f)(g))
-    case MLeaf(v) => g(v,z)
+  def fold[A, B](t: MTree[A])(g: (A)=>B)(f: (B,B)=>B): B = t match {
+    case MBranch(left, right) => f(fold(left)(g)(f), fold(right)(g)(f))
+    case MLeaf(v) => g(v)
   }
 
-  def maximum(t: MTree[Int]): Int = fold(t,0)(_ max _)((v,_)=>v)
+  def maximum(t: MTree[Int]): Int = fold(t)(v=>v)(_ max _)
 
-  def depth[A](t: MTree[A]): Int = fold(t,1)((l,r)=> 1 + l max r)((_,x)=>x)
+  def depth[A](t: MTree[A]): Int = fold(t)(_=>1)(1+_ max _)
 
-  def sum(t: MTree[Int]): Int = fold(t,0)(_+_)((x,_)=>x)
+  def sum(t: MTree[Int]): Int = fold(t)(x=>x)(_+_)
 
   def map(t: MTree[Int])(f: Int => Int): MTree[Int] = 
-    fold(t, MLeaf(0): MTree[Int])( (left, right) => MBranch(left, right) )( (v, _) => MLeaf(f(v)) )
+    fold(t)(v=>MLeaf(f(v)): MTree[Int])(MBranch(_,_))
 }
 
