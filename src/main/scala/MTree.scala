d354c8982406aa245086890befb83c17778356fe
OT Chen <ot32em@gmail.com>
2014-12-04 14:03:54 +0800

    move fold to bottom

--- src/main/scala/MTree.scala
+++ src/main/scala/MTree.scala
@@ -5,11 +5,6 @@ case class MBranch[A](lt: MTree[A], rt: MTree[A]) extends MTree[A]
 object MTree {
   def node_count[A](t: MTree[A]): Int = fold(t)(a=>1)(1+_+_) 
 
-  def fold[A, B](t: MTree[A])(g: (A)=>B)(f: (B,B)=>B): B = t match {
-    case MBranch(left, right) => f(fold(left)(g)(f), fold(right)(g)(f))
-    case MLeaf(v) => g(v)
-  }
-
   def maximum(t: MTree[Int]): Int = fold(t)(v=>v)(_ max _)
 
   def depth[A](t: MTree[A]): Int = fold(t)(_=>1)(1+_ max _)
@@ -18,5 +13,10 @@ object MTree {
 
   def map(t: MTree[Int])(f: Int => Int): MTree[Int] = 
     fold(t)(v=>MLeaf(f(v)): MTree[Int])(MBranch(_,_))
+
+  def fold[A, B](t: MTree[A])(g: (A)=>B)(f: (B,B)=>B): B = t match {
+    case MBranch(left, right) => f(fold(left)(g)(f), fold(right)(g)(f))
+    case MLeaf(v) => g(v)
+  }
 }
 
