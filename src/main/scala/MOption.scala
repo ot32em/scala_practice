924be6fe53906a61d098ac04d636f9c08a4d85a0
OT Chen <ot32em@gmail.com>
2014-12-09 21:28:25 +0800

    pass the test of MOption::map

--- src/main/scala/MOption.scala
+++ src/main/scala/MOption.scala
@@ -1,6 +1,10 @@
 sealed trait MOption[+A]
 {
-    def map[B](f: A => B): MOption[B] = MNone
+    def map[B](f: A => B): MOption[B] = this match {
+        case MNone => MNone: MOption[B]
+        case MSome(x) => MSome(f(x))
+    }
+
 }
 case class MSome[A](v: A) extends MOption[A]
 case object MNone extends MOption[Nothing]
