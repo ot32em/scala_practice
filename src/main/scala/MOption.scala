ce0562b99d6e1f98db260f677eecab43f926b2f0
OT Chen <ot32em@gmail.com>
2014-12-09 21:37:33 +0800

    past the test of flatMap

--- src/main/scala/MOption.scala
+++ src/main/scala/MOption.scala
@@ -5,7 +5,10 @@ sealed trait MOption[+A]
         case MSome(x) => MSome(f(x))
     }
 
-    def flatMap[B](f: A => MOption[B]): MOption[B] = MNone
+    def flatMap[B](f: A => MOption[B]): MOption[B] = this match {
+        case MNone => MNone
+        case MSome(x) => f(x)
+    }
 }
 case class MSome[A](v: A) extends MOption[A]
 case object MNone extends MOption[Nothing]
