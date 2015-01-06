635eff204c49a6a5ca00c09df58ae1139b8e4346
OT Chen <ot32em@gmail.com>
2015-01-06 21:00:37 +0800

    pass the MOption variance test

--- src/main/scala/MOption.scala
+++ src/main/scala/MOption.scala
@@ -31,5 +31,12 @@ case object MNone extends MOption[Nothing]
 
 object MOption
 {
-    def variance(as: Seq[Double]): MOption[Double] = MNone
+    def variance(as: Seq[Double]): MOption[Double] = {
+        def mean(as: Seq[Double]): Double = {
+            as.foldLeft(0.0)(_+_) / as.size
+        }
+        if(as.isEmpty) MNone
+        else MSome(mean(as.map(v => math.pow(v - mean(as), 2))))
+    }
+
 }
