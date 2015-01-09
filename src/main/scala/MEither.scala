824e385daf0d58cebd22842172df6767a6349385
OT Chen <ot32em@gmail.com>
2015-01-09 14:06:31 +0800

    prapare the testing empty case for exe 4.6 and 4.7

---
+++ src/main/scala/MEither.scala
@@ -0,0 +1,15 @@
+trait MEither[+E, +A] {
+    def map[B](f: A => B): MEither[E, B] = null
+    def flatMap[EE >: E, B](f: A => MEither[EE, B]): MEither[EE, B] = null
+    def orElse[EE >: E, B >: A](b: => MEither[EE, B]): MEither[EE, B] = null
+    def map2[EE >: E, B, C](b: MEither[EE, B])(f: (A, B) => C): MEither[EE, C] = null
+}
+case class MRight[+A](v: A) extends MEither[Nothing, A]
+case class MLeft[+E](e: E) extends MEither[E, Nothing]
+
+object MEither
+{
+    def sequence[E, A](as: List[MEither[E, A]]): MEither[E, List[A]] = null
+    def traverse[E, A, B](as: List[MEither[E, A]])(
+                          f: A => MEither[E, B]): MEither[E, List[B]] = null
+}
---
+++ src/test/scala/MEitherTest.scala
@@ -0,0 +1,44 @@
+import org.scalatest._
+import org.scalatest.Matchers._
+import MEither._
+
+class MEitherTest extends FunSuite with Matchers
+{
+    test("exe 4.6 map") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.6 map none case") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.6 flatMap") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.6 flatMap none case") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.6 orElse") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.6 orElse none case") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.6 map2") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.6 map2 none case") {
+        fail("test not implemeneted")
+    }
+
+    test("exe 4.7 sequence") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.7 sequence none case") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.7 traverse") {
+        fail("test not implemeneted")
+    }
+    test("exe 4.7 traverse none case") {
+        fail("test not implemeneted")
+    }
+}
