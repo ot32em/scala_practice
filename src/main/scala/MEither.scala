da59922c9a25227667598e05edc1f4ab07d9d19b
OT Chen <ot32em@gmail.com>
2015-01-12 22:17:31 +0800

    pass the map of meither

--- src/main/scala/MEither.scala
+++ src/main/scala/MEither.scala
@@ -1,5 +1,9 @@
 trait MEither[+E, +A] {
-    def map[B](f: A => B): MEither[E, B] = null
+    def map[B](f: A => B): MEither[E, B] = this match {
+        case MRight(v) => MRight(f(v))
+        case MLeft(err) => MLeft(err)
+    }
+
     def flatMap[EE >: E, B](f: A => MEither[EE, B]): MEither[EE, B] = null
     def orElse[EE >: E, B >: A](b: => MEither[EE, B]): MEither[EE, B] = null
     def map2[EE >: E, B, C](b: MEither[EE, B])(f: (A, B) => C): MEither[EE, C] = null
