f82b153d2d1627dab40e182341ec6752f2024640
OT Chen <ot32em@gmail.com>
2015-01-12 22:31:35 +0800

    pass the test of orElse

--- src/main/scala/MEither.scala
+++ src/main/scala/MEither.scala
@@ -9,7 +9,11 @@ trait MEither[+E, +A] {
         case MLeft(er) => MLeft(er)
     }
 
-    def orElse[EE >: E, B >: A](b: => MEither[EE, B]): MEither[EE, B] = null
+    def orElse[EE >: E, B >: A](b: => MEither[EE, B]): MEither[EE, B] = this match {
+        case MRight(v) => this
+        case MLeft(err) => b
+    }
+
     def map2[EE >: E, B, C](b: MEither[EE, B])(f: (A, B) => C): MEither[EE, C] = null
 }
 case class MRight[+A](v: A) extends MEither[Nothing, A]
